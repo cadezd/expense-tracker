b5ca8b3fb98d35601a46ad105dcd4332bc275775
David Čadež <david.cadez89@gmail.com>
2026-07-14 12:28:37 +0200

    Added placeholder mthods in transaction repository.

--- backend/internal/transaction/transaction_repostory.go
+++ backend/internal/transaction/transaction_repostory.go
@@ -2,6 +2,7 @@ package transaction
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -13,4 +14,53 @@ type TransactionRepository interface {
 	GetByID(ctx context.Context, userID, transactionID uuid.UUID) (*Transaction, error)
 	List(ctx context.Context, userID uuid.UUID, offset, limit int) ([]*Transaction, error)
 	Delete(ctx context.Context, userID, transactionID uuid.UUID) error
+}
+
+type PostgresTransactionRepository struct {
+	db *pgxpool.Pool
+}
+
+func NewPostgresTransactionRepository(db *pgxpool.Pool) *PostgresTransactionRepository {
+	return &PostgresTransactionRepository{
+		db: db,
+	}
+}
+
+func (tr *PostgresTransactionRepository) Create(
+	ctx context.Context,
+	transaction *Transaction,
+) (*Transaction, error) {
+	return nil, fmt.Errorf("Not implemented")
+}
+
+func (tr *PostgresTransactionRepository) Update(
+	ctx context.Context,
+	transaction *Transaction,
+) (*Transaction, error) {
+	return nil, fmt.Errorf("Not implemented")
+}
+
+func (tr *PostgresTransactionRepository) GetByID(
+	ctx context.Context,
+	userID uuid.UUID,
+	transactionID uuid.UUID,
+) (*Transaction, error) {
+	return nil, fmt.Errorf("Not implemented")
+}
+
+func (tr *PostgresTransactionRepository) List(
+	ctx context.Context,
+	userID uuid.UUID,
+	offset int,
+	limit int,
+) ([]*Transaction, error) {
+	return nil, fmt.Errorf("Not implemented")
+}
+
+func (tr *PostgresTransactionRepository) Delete(
+	ctx context.Context,
+	userID uuid.UUID,
+	transactionID uuid.UUID,
+) error {
+	return fmt.Errorf("Not implemented")
 }
