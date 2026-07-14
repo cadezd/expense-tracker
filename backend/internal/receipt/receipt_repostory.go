ac6ff59de6af447fcca389323d3854f06f39ae34
David Čadež <david.cadez89@gmail.com>
2026-07-14 12:26:35 +0200

    Updated transaction repostory - switched to pointers and uuids instead of strings.

--- backend/internal/receipt/receipt_repostory.go
+++ backend/internal/receipt/receipt_repostory.go
@@ -4,12 +4,13 @@ import (
 	"context"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type ReceiptRepository interface {
-	Crate(ctx context.Context, receipt Receipt) (Receipt, error)
-	Update(ctx context.Context, receipt Receipt) (Receipt, error)
-	GetByID(ctx context.Context, userID, receiptID uuid.UUID) (Receipt, error)
-	List(ctx context.Context, userID string, offset, limit int) ([]Receipt, error)
+	Create(ctx context.Context, receipt *Receipt) (*Receipt, error)
+	Update(ctx context.Context, receipt *Receipt) (*Receipt, error)
+	GetByID(ctx context.Context, userID, receiptID uuid.UUID) (*Receipt, error)
+	List(ctx context.Context, userID uuid.UUID, offset, limit int) ([]*Receipt, error)
 	Delete(ctx context.Context, userID, receiptID uuid.UUID) error
 }
