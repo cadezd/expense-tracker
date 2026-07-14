fb8ba9764a89acc8fb97d706e3bc39612cbb0748
David Čadež <david.cadez89@gmail.com>
2026-07-14 12:24:05 +0200

    Added placeholder methods in user repository.

--- backend/internal/user/user_repostory.go
+++ backend/internal/user/user_repostory.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -13,4 +14,50 @@ type UserRepository interface {
 	GetByID(ctx context.Context, userID uuid.UUID) (*User, error)
 	List(ctx context.Context, offset, limit int) ([]*User, error)
 	Delete(ctx context.Context, userID uuid.UUID) error
+}
+
+type PostgresUserRepository struct {
+	db *pgxpool.Pool
+}
+
+func NewPostgresUserRepository(db *pgxpool.Pool) *PostgresUserRepository {
+	return &PostgresUserRepository{
+		db: db,
+	}
+}
+
+func (ur *PostgresUserRepository) Create(
+	ctx context.Context,
+	user *User,
+) (*User, error) {
+	return nil, fmt.Errorf("Not implemented")
+}
+
+func (ur *PostgresUserRepository) Update(
+	ctx context.Context,
+	user *User,
+) (*User, error) {
+	return nil, fmt.Errorf("Not implemented")
+}
+
+func (ur *PostgresUserRepository) GetByID(
+	ctx context.Context,
+	userID uuid.UUID,
+) (*User, error) {
+	return nil, fmt.Errorf("Not implemented")
+}
+
+func (ur *PostgresUserRepository) List(
+	ctx context.Context,
+	offset int,
+	limit int,
+) ([]*User, error) {
+	return nil, fmt.Errorf("Not implemented")
+}
+
+func (ur *PostgresUserRepository) Delete(
+	ctx context.Context,
+	userID uuid.UUID,
+) error {
+	return fmt.Errorf("Not implemented")
 }
