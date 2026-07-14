a9f0cb4c4519a11c6bab7306e71c730df61d1e12
David Čadež <david.cadez89@gmail.com>
2026-07-14 12:28:09 +0200

    Updated transaction repostory - switched to pointers and to uuids instead of strings.

--- backend/internal/transaction/transaction_repostory.go
+++ backend/internal/transaction/transaction_repostory.go
@@ -4,12 +4,13 @@ import (
 	"context"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type TransactionRepostory interface {
-	Crate(ctx context.Context, transaction Transaction) (Transaction, error)
-	Update(ctx context.Context, transaction Transaction) (Transaction, error)
-	GetByID(ctx context.Context, userID, transactionID uuid.UUID) (Transaction, error)
-	List(ctx context.Context, userID uuid.UUID, offset, limit int) ([]Transaction, error)
+type TransactionRepository interface {
+	Create(ctx context.Context, transaction *Transaction) (*Transaction, error)
+	Update(ctx context.Context, transaction *Transaction) (*Transaction, error)
+	GetByID(ctx context.Context, userID, transactionID uuid.UUID) (*Transaction, error)
+	List(ctx context.Context, userID uuid.UUID, offset, limit int) ([]*Transaction, error)
 	Delete(ctx context.Context, userID, transactionID uuid.UUID) error
 }
