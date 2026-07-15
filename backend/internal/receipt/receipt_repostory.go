44d741777faf68fb77cb9e8cb33370c85a810ab5
David Čadež <david.cadez89@gmail.com>
2026-07-15 20:53:37 +0200

    Implementing receipt repository.

--- backend/internal/receipt/receipt_repostory.go
+++ backend/internal/receipt/receipt_repostory.go
@@ -2,15 +2,18 @@ package receipt
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
+	"github.com/cadezd/expense-tracker/internal/common"
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type ReceiptRepository interface {
-	Create(ctx context.Context, receipt *Receipt) (*Receipt, error)
-	Update(ctx context.Context, receipt *Receipt) (*Receipt, error)
+	Create(ctx context.Context, receipt *Receipt) error
+	Update(ctx context.Context, receipt *Receipt) error
 	GetByID(ctx context.Context, userID, receiptID uuid.UUID) (*Receipt, error)
 	List(ctx context.Context, userID uuid.UUID, offset, limit int) ([]*Receipt, error)
 	Delete(ctx context.Context, userID, receiptID uuid.UUID) error
@@ -29,15 +32,84 @@ func NewPostgresReceiptRepository(db *pgxpool.Pool) *PostgresReceiptRepository {
 func (rr *PostgresReceiptRepository) Create(
 	ctx context.Context,
 	receipt *Receipt,
-) (*Receipt, error) {
-	return nil, fmt.Errorf("Not implemented")
+) error {
+	sqlQuery := `
+		INSERT INTO receipts (user_id, original_filename, stored_filename, storage_path, mime_type, file_size)
+		VALUES ($1, $2, $3, $4, $5, $6)
+		RETURNING id, status, created_at, updated_at, object_version;
+	`
+
+	err := rr.db.QueryRow(
+		ctx,
+		sqlQuery,
+		receipt.UserID,
+		receipt.OriginalFilename,
+		receipt.StoredFilename,
+		receipt.StoragePath,
+		receipt.MimeType,
+		receipt.FileSize,
+	).Scan(
+		&receipt.ID,
+		&receipt.Status,
+		&receipt.CreatedAt,
+		&receipt.UpdatedAt,
+		&receipt.ObjectVersion,
+	)
+	if err != nil {
+		return fmt.Errorf("create receipt error: %w", err)
+	}
+
+	return nil
 }
 
 func (rr *PostgresReceiptRepository) Update(
 	ctx context.Context,
 	receipt *Receipt,
-) (*Receipt, error) {
-	return nil, fmt.Errorf("Not implemented")
+) error {
+	sqlQuery := `
+		UPDATE receipts
+		SET 
+			original_filename = $1,
+			stored_filename = $2,
+			storage_path = $3,
+			mime_type = $4,
+			file_size = $5,
+			status = $6,
+			updated_at = NOW(),
+			object_version = object_version + 1
+		WHERE
+			user_id = $7 AND
+			id = $8 AND
+			object_version = $9
+		RETURNING
+			updated_at,
+			object_version;
+	`
+
+	err := rr.db.QueryRow(
+		ctx,
+		sqlQuery,
+		receipt.OriginalFilename,
+		receipt.StoredFilename,
+		receipt.StoragePath,
+		receipt.MimeType,
+		receipt.FileSize,
+		receipt.Status,
+		receipt.UserID,
+		receipt.ID,
+		receipt.ObjectVersion,
+	).Scan(
+		&receipt.UpdatedAt,
+		&receipt.ObjectVersion,
+	)
+	if errors.Is(err, pgx.ErrNoRows) {
+		return common.ErrNotFound
+	}
+	if err != nil {
+		return fmt.Errorf("update receipt error: %w", err)
+	}
+
+	return nil
 }
 
 func (rr *PostgresReceiptRepository) GetByID(
@@ -45,7 +117,53 @@ func (rr *PostgresReceiptRepository) GetByID(
 	userID uuid.UUID,
 	receiptID uuid.UUID,
 ) (*Receipt, error) {
-	return nil, fmt.Errorf("Not implemented")
+	receipt := &Receipt{}
+
+	sqlQuery := `
+		SELECT 
+			id,
+			user_id,
+			original_filename,
+			stored_filename,
+			storage_path,
+			mime_type,
+			file_size,
+			status,
+			created_at,
+			updated_at,
+			object_version
+		FROM receipts
+		WHERE 
+			user_id = $1 AND 
+			id = $2;
+	`
+
+	err := rr.db.QueryRow(
+		ctx,
+		sqlQuery,
+		userID,
+		receiptID,
+	).Scan(
+		&receipt.ID,
+		&receipt.UserID,
+		&receipt.OriginalFilename,
+		&receipt.StoredFilename,
+		&receipt.StoragePath,
+		&receipt.MimeType,
+		&receipt.FileSize,
+		&receipt.Status,
+		&receipt.CreatedAt,
+		&receipt.UpdatedAt,
+		&receipt.ObjectVersion,
+	)
+	if errors.Is(err, pgx.ErrNoRows) {
+		return nil, common.ErrNotFound
+	}
+	if err != nil {
+		return nil, fmt.Errorf("get by id error: %w", err)
+	}
+
+	return receipt, nil
 }
 
 func (rr *PostgresReceiptRepository) List(
@@ -54,7 +172,67 @@ func (rr *PostgresReceiptRepository) List(
 	offset int,
 	limit int,
 ) ([]*Receipt, error) {
-	return nil, fmt.Errorf("Not implemented")
+	sqlQuery := `
+		SELECT 
+			id,
+			user_id,
+			original_filename,
+			stored_filename,
+			storage_path,
+			mime_type,
+			file_size,
+			status,
+			created_at,
+			updated_at,
+			object_version
+		FROM receipts
+		WHERE 
+			user_id = $1
+		ORDER BY created_at DESC
+		LIMIT $2
+		OFFSET $3;
+	`
+
+	rows, err := rr.db.Query(
+		ctx,
+		sqlQuery,
+		userID,
+		limit,
+		offset,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("list receipts error: %w", err)
+	}
+	defer rows.Close()
+
+	receipts := make([]*Receipt, 0)
+	for rows.Next() {
+		receipt := &Receipt{}
+
+		err := rows.Scan(
+			&receipt.ID,
+			&receipt.UserID,
+			&receipt.OriginalFilename,
+			&receipt.StoredFilename,
+			&receipt.StoragePath,
+			&receipt.MimeType,
+			&receipt.FileSize,
+			&receipt.Status,
+			&receipt.CreatedAt,
+			&receipt.UpdatedAt,
+			&receipt.ObjectVersion,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("scan receipt error: %w", err)
+		}
+
+		receipts = append(receipts, receipt)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("iterate receipts error: %w", err)
+	}
+
+	return receipts, nil
 }
 
 func (rr *PostgresReceiptRepository) Delete(
@@ -62,5 +240,23 @@ func (rr *PostgresReceiptRepository) Delete(
 	userID uuid.UUID,
 	receiptID uuid.UUID,
 ) error {
-	return fmt.Errorf("Not implemented")
+	sqlQuery := `
+		DELETE 
+		FROM receipts 
+		WHERE 
+			user_id = $1 AND 
+			id = $2;
+	`
+
+	_, err := rr.db.Exec(
+		ctx,
+		sqlQuery,
+		userID,
+		receiptID,
+	)
+	if err != nil {
+		return fmt.Errorf("delete receipt error: %w", err)
+	}
+
+	return nil
 }
