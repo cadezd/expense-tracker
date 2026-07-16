18c78b85f4ff864d31c44d6e8f33966d23892184
David Čadež <david.cadez89@gmail.com>
2026-07-16 10:21:05 +0200

    Implemnted transaction repository.

--- backend/internal/transaction/transaction_repostory.go
+++ backend/internal/transaction/transaction_repostory.go
@@ -2,15 +2,18 @@ package transaction
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
+	"github.com/cadezd/expense-tracker/internal/common"
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type TransactionRepository interface {
-	Create(ctx context.Context, transaction *Transaction) (*Transaction, error)
-	Update(ctx context.Context, transaction *Transaction) (*Transaction, error)
+	Create(ctx context.Context, transaction *Transaction) error
+	Update(ctx context.Context, transaction *Transaction) error
 	GetByID(ctx context.Context, userID, transactionID uuid.UUID) (*Transaction, error)
 	List(ctx context.Context, userID uuid.UUID, offset, limit int) ([]*Transaction, error)
 	Delete(ctx context.Context, userID, transactionID uuid.UUID) error
@@ -29,15 +32,86 @@ func NewPostgresTransactionRepository(db *pgxpool.Pool) *PostgresTransactionRepo
 func (tr *PostgresTransactionRepository) Create(
 	ctx context.Context,
 	transaction *Transaction,
-) (*Transaction, error) {
-	return nil, fmt.Errorf("Not implemented")
+) error {
+	sqlQuery := `
+		INSERT INTO transactions (user_id, receipt_id, type, counterparty, amount, currency, transaction_date, category, description)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+		RETURNING id, created_at, updated_at, object_version;
+	`
+
+	err := tr.db.QueryRow(ctx, sqlQuery,
+		transaction.UserID,
+		transaction.ReceiptID,
+		transaction.Type,
+		transaction.Counterparty,
+		transaction.Amount,
+		transaction.Currency,
+		transaction.TransactionDate,
+		transaction.Category,
+		transaction.Description,
+	).Scan(
+		&transaction.ID,
+		&transaction.CreatedAt,
+		&transaction.UpdatedAt,
+		&transaction.ObjectVersion,
+	)
+	if err != nil {
+		return fmt.Errorf("transaction create error: %w", err)
+	}
+
+	return nil
 }
 
 func (tr *PostgresTransactionRepository) Update(
 	ctx context.Context,
 	transaction *Transaction,
-) (*Transaction, error) {
-	return nil, fmt.Errorf("Not implemented")
+) error {
+	sqlQuery := `
+		UPDATE transactions
+		SET
+			receipt_id = $1,
+			type = $2,
+			counterparty = $3,
+			amount = $4,
+			currency = $5,
+			transaction_date = $6,
+			category = $7,
+			description = $8,
+			updated_at = NOW(),
+			object_version = object_version + 1
+		WHERE
+			user_id = $9 AND
+			id = $10 AND
+			object_version = $11
+		RETURNING
+			updated_at,
+			object_version;
+	`
+
+	err := tr.db.QueryRow(ctx, sqlQuery,
+		transaction.ReceiptID,
+		transaction.Type,
+		transaction.Counterparty,
+		transaction.Amount,
+		transaction.Currency,
+		transaction.TransactionDate,
+		transaction.Category,
+		transaction.Description,
+		transaction.UserID,
+		transaction.ID,
+		transaction.ObjectVersion,
+	).Scan(
+		&transaction.UpdatedAt,
+		&transaction.ObjectVersion,
+	)
+	if errors.Is(err, pgx.ErrNoRows) {
+		return common.ErrNotFound
+	}
+	if err != nil {
+		return fmt.Errorf("update transaction error: %w", err)
+	}
+
+	return nil
 }
 
 func (tr *PostgresTransactionRepository) GetByID(
@@ -45,7 +119,56 @@ func (tr *PostgresTransactionRepository) GetByID(
 	userID uuid.UUID,
 	transactionID uuid.UUID,
 ) (*Transaction, error) {
-	return nil, fmt.Errorf("Not implemented")
+	transaction := &Transaction{}
+
+	sqlQuery := `
+		SELECT
+			id,
+			user_id,
+			receipt_id,
+			type,
+			counterparty,
+			amount,
+			currency,
+			transaction_date,
+			category,
+			description,
+			created_at,
+			updated_at,
+			object_version
+		FROM 
+			transactions
+		WHERE
+			user_id = $1 AND
+			id = $2;
+	`
+
+	err := tr.db.QueryRow(ctx, sqlQuery,
+		userID,
+		transactionID,
+	).Scan(
+		&transaction.ID,
+		&transaction.UserID,
+		&transaction.ReceiptID,
+		&transaction.Type,
+		&transaction.Counterparty,
+		&transaction.Amount,
+		&transaction.Currency,
+		&transaction.TransactionDate,
+		&transaction.Category,
+		&transaction.Description,
+		&transaction.CreatedAt,
+		&transaction.UpdatedAt,
+		&transaction.ObjectVersion,
+	)
+	if errors.Is(err, pgx.ErrNoRows) {
+		return nil, common.ErrNotFound
+	}
+	if err != nil {
+		return nil, fmt.Errorf("get transaction by id error: %w", err)
+	}
+
+	return transaction, nil
 }
 
 func (tr *PostgresTransactionRepository) List(
@@ -54,7 +177,70 @@ func (tr *PostgresTransactionRepository) List(
 	offset int,
 	limit int,
 ) ([]*Transaction, error) {
-	return nil, fmt.Errorf("Not implemented")
+	sqlQuery := `
+		SELECT
+			id,
+			user_id,
+			receipt_id,
+			type,
+			counterparty,
+			amount,
+			currency,
+			transaction_date,
+			category,
+			description,
+			created_at,
+			updated_at,
+			object_version
+		FROM 
+			transactions
+		WHERE
+			user_id = $1
+		ORDER BY created_at DESC
+		LIMIT $2
+		OFFSET $3;
+	`
+
+	rows, err := tr.db.Query(ctx, sqlQuery,
+		userID,
+		limit,
+		offset,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("list transactions error: %w", err)
+	}
+	defer rows.Close()
+
+	transactions := make([]*Transaction, 0)
+	for rows.Next() {
+
+		transaction := &Transaction{}
+		err := rows.Scan(
+			&transaction.ID,
+			&transaction.UserID,
+			&transaction.ReceiptID,
+			&transaction.Type,
+			&transaction.Counterparty,
+			&transaction.Amount,
+			&transaction.Currency,
+			&transaction.TransactionDate,
+			&transaction.Category,
+			&transaction.Description,
+			&transaction.CreatedAt,
+			&transaction.UpdatedAt,
+			&transaction.ObjectVersion,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("scan transaction error: %w", err)
+		}
+
+		transactions = append(transactions, transaction)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("iterate transactions error: %w", err)
+	}
+
+	return transactions, nil
 }
 
 func (tr *PostgresTransactionRepository) Delete(
@@ -62,5 +248,21 @@ func (tr *PostgresTransactionRepository) Delete(
 	userID uuid.UUID,
 	transactionID uuid.UUID,
 ) error {
-	return fmt.Errorf("Not implemented")
+	sqlQuery := `
+		DELETE 
+		FROM transactions
+		WHERE 
+			user_id = $1 AND
+			id = $2;
+	`
+
+	_, err := tr.db.Exec(ctx, sqlQuery,
+		userID,
+		transactionID,
+	)
+	if err != nil {
+		return fmt.Errorf("delete transation error: %w", err)
+	}
+
+	return nil
 }
