27fca441fbd3e5374740fa9a751d950ed53d0a84
David Čadež <david.cadez89@gmail.com>
2026-07-23 13:14:41 +0200

    Moved errors to different file.

---
+++ backend/internal/transaction/errors.go
@@ -0,0 +1,13 @@
+package transaction
+
+import "errors"
+
+var (
+	ErrEmptyRequest             = errors.New("invalid empty request")
+	ErrInvalidType              = errors.New("invalid transaction type")
+	ErrInvalidAmount            = errors.New("invalid transaction amount")
+	ErrNegativeAmount           = errors.New("negative transaction amount")
+	ErrInvalidCurrency          = errors.New("invalid transaction currency")
+	ErrInvalidTransactionDate   = errors.New("invalid transaction date")
+	ErrReceiptOwnershipMismatch = errors.New("receipt belongs to a different user")
+)
